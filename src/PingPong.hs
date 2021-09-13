881e2a524e809bff0ce94a7efa428df682f05535
Pieter Michels <pieter.michels.nl@gmail.com>
2021-09-13 13:24:04 +0200

    Remove use of Socketplayer as I do not have python

--- src/PingPong.hs
+++ src/PingPong.hs
@@ -7,8 +7,8 @@ import           PingPong.Simulation.Realtime
 
 import           PingPong.Player
 import qualified PingPong.Player.NativePlayer as NativePlayer
-import qualified PingPong.Player.ArgPlayer    as ArgPlayer
-import qualified PingPong.Player.SocketPlayer as SocketPlayer
+--import qualified PingPong.Player.ArgPlayer    as ArgPlayer
+--import qualified PingPong.Player.SocketPlayer as SocketPlayer
 
 main :: IO ()
-main = play SocketPlayer.player NativePlayer.player
+main = play NativePlayer.player NativePlayer.player
