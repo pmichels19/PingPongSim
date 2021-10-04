f7506fb79073dc7e142402e30db4e7331f60cb85
Pieter Michels <pieter.michels.nl@gmail.com>
2021-10-04 13:17:17 +0200

    use solobolo player

--- src/PingPong.hs
+++ src/PingPong.hs
@@ -6,13 +6,14 @@ import           PingPong.Model
 import           PingPong.Simulation.Realtime
 
 import           PingPong.Player
+import qualified PingPong.Player.SoloboloPlayer as SoloboloPlayer
 import qualified PingPong.Player.NativePlayer as NativePlayer
 import qualified PingPong.Player.ArgPlayer    as ArgPlayer
 import qualified PingPong.Player.SocketPlayer as SocketPlayer
 
 main :: IO ()
 --main = play SocketPlayer.player NativePlayer.player
-main = playWithCollision NativePlayer.collision 
-       SocketPlayer.player NativePlayer.player
+main = playWithCollision SoloboloPlayer.collision 
+       SoloboloPlayer.player NativePlayer.player
 
        
--- src/PingPong/Player/SoloboloPlayer.hs
+++ src/PingPong/Player/SoloboloPlayer.hs
@@ -1,17 +1,20 @@
-module PingPong.Player.TestPlayer (player) where
+module PingPong.Player.SoloboloPlayer (player, collision) where
 
 import Control.Lens
 import Data.Geometry
+import Data.Ext
 
 import PingPong.Model
 import PingPong.Player
+
+import PingPong.Simulation.Collision
 
 import Data.Colour
 import Data.Colour.Names
 
 player :: Player
 player = defaultPlayer
-  { name    = "Pieters' Player"
+  { name    = "Solobolo"
   , arm     = wavyArm
   , foot    = wavyFoot
   , action  = wavyAction
@@ -54,3 +57,16 @@ wavyAction t hit ballState arm = do
             , joint4Rotation
            ]
 
+-- Check for collision between a moving point p and a moving line segment qr, 
+-- given their locations at two different times.
+-- If a collision occurs, return the time of collision, the location of p
+-- at collision time, and the resulting velocity vector of p at that time.
+collision :: CollisionChecker
+collision (time1, point1, segment1) (time2, point2, segment2) = do
+  let Point2 xp1 yp1 = point1
+      Point2 xp2 yp2 = point2
+      Point2 xq1 yq1 = segment1 ^. start ^. core  
+      Point2 xq2 yq2 = segment2 ^. start ^. core
+      Point2 xr1 yr1 = segment1 ^. end   ^. core
+      Point2 xr2 yr2 = segment2 ^. end   ^. core
+  return Nothing
